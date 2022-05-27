6e92bc8e70209b0376532d33a2a98fae22d7e5bf
NEARLABS <89186935+NEARLABS@users.noreply.github.com>
2022-05-27 23:30:52 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "https://i.pinimg.com/originals/9b/0b/e4/9b0be4f4e069e7c8152d4c7c9e472edc.jpg",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "https://i.pinimg.com/originals/15/47/5b/15475bb2f7ba19c471b8e079593bff2f.jpg",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "https://1.bp.blogspot.com/-4dPqmx7ZNc0/XdJ8jEEsH1I/AAAAAAAAFaQ/_efdcHed-psDxrZ7E1eGO_vfmsnpGc0VgCLcBGAsYHQ/s1600/20191118_174246.jpg",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "https://i.pinimg.com/originals/15/47/5b/15475bb2f7ba19c471b8e079593bff2f.jpg",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "https://i.pinimg.com/originals/15/47/5b/15475bb2f7ba19c471b8e079593bff2f.jpg",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "https://i.pinimg.com/originals/15/47/5b/15475bb2f7ba19c471b8e079593bff2f.jpg",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
